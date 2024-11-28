d3bf435528b1699cf1c4fc50926556ecbca10d8b
allykotetsu <arisudotexe@proton.me>
2024-11-28 12:49:05 -0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 cargo build --package enchat --release --bin install
 cargo build --package enchat --release --bin gen_db
 cargo build --package enchat --release --bin system_service
-cargo build --package enchat --release --bin messenger
+cargo build --package enchat --release --bin enchat
 ./target/release/install
 ./target/release/gen_db
 ./target/release/system_service
